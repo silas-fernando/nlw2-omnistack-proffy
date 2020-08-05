2f12b2e9a34858a273f4a01e2d8e32a839336dd6
Silas-fp <silasf_primiani@hotmail.com>
2020-08-05 00:59:46 -0300

    Criando estrutura da landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import Landing from './pages/Landing'
+
 import './assets/styles/global.css';
 
 // JSX = JavaScript + XML
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Landing />
   );
 }
 
