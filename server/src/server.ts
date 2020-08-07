d8d43ab9cd44d36d0f22753051a788a8fa279cff
Silas-fp <silasf_primiani@hotmail.com>
2020-08-07 00:04:43 -0300

    Explicando conceitos: Rotas e recursos, métodos HTTP e Parâmetros

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,25 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+// Métodos HTTP:
+// GET: Buscar ou listar uma informação.
+// POST: Criar alguma nova informação.
+// PUT: Atualizar uma informação existente.
+// DELETE: Deletar uma informação existente.
+
+// Corpo (Request Body) Dados para criação ou atualização de um registro.
+// Route Params: Identifica qual recurso deverá ser atualizado ou deletado.
+// Query Params: Paginação, filtros, ordenação, etc.
+
 app.get('/users', (request, response) => {
+  console.log(request.query);
+
   const users = [
     { name: 'Diego', age: 25 },
     { name: 'Vini', age: 21 },
+    { name: 'Berranteiro', age: 300 },
   ];
   
   return response.json(users);
