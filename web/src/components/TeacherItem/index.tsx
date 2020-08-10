a358609d7d4d4db1d73f30de653d21822a893f4f
Silas-fp <silasf_primiani@hotmail.com>
2020-08-10 20:30:58 -0300

    Carregando total de conexões na Landing, cadastro de novas aulas e listagem de professores

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,10 +3,60 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import beatrizThome from '../../assets/images/beatriz-thome.png';
 
+import api from 'src/services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
+    <article className="teacher-item">
+      <header>
+          <img src={teacher.avatar} alt={teacher.name} />
+          <div>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
+          </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+          <p>
+              Preço/Hora
+              <strong>R$ {teacher.cost}</strong>
+          </p>
+          <a 
+            target="_blank" 
+            onClick={createNewConnection} 
+            href={`https://wa.me/${teacher.whatsapp}`}
+          >
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+          </a>
+      </footer>
+    </article>
+    
+    /*
     <article className="teacher-item">
       <header>
           <img src={beatrizThome} alt="Beatriz Thomé"/>
@@ -33,6 +83,7 @@ function TeacherItem() {
           </button>
       </footer>
     </article>
+    */
   )
 }
 
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -7,10 +7,23 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from 'src/services/api';
+
 import './styles.css';
 import { Link } from 'react-router-dom';
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0); 
+
+    // Executa a função passada no primeiro parâmetro toda vez que o valor do segundo parâmetro alterado na nossa api.
+    useEffect(() => {
+      api.get('connections').then(response => {
+          const { total } = response.data;
+
+          setTotalConnections(total);
+      })
+    }, []); 
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -38,7 +51,7 @@ function Landing() {
                 </div> 
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/> 
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/> 
                 </span>
             </div>
         </div>
