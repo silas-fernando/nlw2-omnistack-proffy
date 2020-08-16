b2f94b9e3dc3e578ccaca137b7626e1cf3923410
Silas-fp <silasf_primiani@hotmail.com>
2020-08-16 18:13:23 -0300

    Navegação da Landing PageMobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Landing from './src/pages/Landing';
-import { AppLoading } from 'expo'; // Mostra uma imagem para o usuário que indica que o app esta carregando.
+import React from "react";
+import { StatusBar } from "react-native";
+import { AppLoading } from "expo"; // Mostra uma imagem para o usuário que indica que o app esta carregando.
 
-  import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
-import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from "@expo-google-fonts/archivo";
+
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
+
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -14,14 +23,15 @@ export default function App() {
     Poppins_600SemiBold,
   });
 
-  if (!fontsLoaded) { // Enquanto as fontes não forem carregadas mostra o AppLoading.
+  if (!fontsLoaded) {
+    // Enquanto as fontes não forem carregadas mostra o AppLoading.
     return <AppLoading />;
   } else {
     return (
       //<></> = Fragment. É como se fosse uma div, mas ela não é passada para o html, é uma tag sem conteúdo html.
-      <> 
-        <Landing />
-        <StatusBar style="light" />
+      <>
+        <AppStack />
+        <StatusBar barStyle="light-content" />
       </>
     );
   }
